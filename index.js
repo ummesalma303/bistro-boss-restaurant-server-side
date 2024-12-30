03ba3630ab64f298a792901c16bf00f682fc6052
ummesalma303 <ummesalma333999@gmail.com>
2024-12-30 16:33:45 +0600

    Added menu and review get api

--- index.js
+++ index.js
@@ -29,6 +29,23 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
+
+
+    app.get('/menu',async(req,res)=>{
+    const result =await menuCollection.find().toArray();
+    res.send(result)
+    })
+
+    app.get('/reviews',async(req,res)=>{
+    const result =await reviewCollection.find().toArray();
+    res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
