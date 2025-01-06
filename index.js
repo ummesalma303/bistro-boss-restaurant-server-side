a279fc568ace3f27cc616ba586a3f9815de253cd
ummesalma303 <ummesalma333999@gmail.com>
2025-01-06 13:38:50 +0600

    Added admin api

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 //middleware
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 app.use(express.json());
 
 
@@ -39,17 +42,76 @@ async function run() {
     const userCollection = client.db("bistroDb").collection("users");
 
 
+    /* ----------------------------------- jwt ---------------------------------- */
+    app.post('/jwt',async (req,res) => {
+      const user = req.body
+      
+      const token = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' });
+      res.send({token})
+    })
 
+ // middlewares 
+ const verifyToken = (req, res, next) =>{
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = req.headers.authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+     if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+     }
+     req.decoded = decoded
+     next()
+  });
+ }
+
+  /* ---------------------------------- admin --------------------------------- */
+  app.get('/users/admin/:email',verifyToken,async (req,res) =>{
+    const email = req.params.email
+    if (email === req.decoded.email) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    const query = {email: email}
+    const user = await userCollection.findOne(query) 
+    let admin = false
+    if (user) {
+      admin= user?.role === 'admin'
+    }
+    res.send({admin})
+  })
+  
+  app.patch('/users/admin/:id',async (req,res)=>{
+    const id = req.params.id
+    const filter ={_id: new ObjectId(id)}
+    const updateDoc = {
+      $set:{
+        role: 'admin'
+      }
+    }
+    const result = await userCollection.updateOne(filter,updateDoc)
+    res.send(result)
+  })
     /* ---------------------------------- users --------------------------------- */
+    app.get('/users',verifyToken,async(req,res)=>{
+      console.log(req.headers.authorization)
+      const result = await userCollection.find().toArray();
+      res.send(result)
+      })
     app.post('/users',async (req,res)=>{
       const users = req.body
+      // console.log(req.headers)
       const result = await userCollection.insertOne(users);
       console.log(result)
       res.send(result)
     })
-
-
-
+    app.delete('/users/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+    
+   
 
     /* ---------------------------------- menu ---------------------------------- */
     app.get('/menu',async(req,res)=>{
